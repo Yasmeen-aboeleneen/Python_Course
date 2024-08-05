dc580cd039087bd8754506c7c8e156962224a1bf
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-08-05 19:08:34 +0300

    Data type

--- first.py
+++ first.py
@@ -1,1 +1,20 @@
+
+# Info About File
 print("I Love Python")
+
+
+
+#--------------------
+#This is a multi line comment
+#--------------------
+
+#type() Built in function to show type of data
+print(type(10))  #int => integer
+print(type("I Love Python"))   #str =>  String
+print(type(10.2))   #Float
+print(type([1,2,3]))  #List
+print(type((1,2,3)))  #Tuple
+print(type({"one":1 , "two":2})) #dict  => Dictionary
+print(type(2==2))  #bool => boolean
+
+
